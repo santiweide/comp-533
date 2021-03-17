bfd55f47ab3c73aa9bf1d387ee9c1006af70aa73
santiweide <ggy_778@sina.com>
2021-03-17 23:09:00 +0800

    basic map counter

--- src/tasks/a1/TokenCounter.java
+++ src/tasks/a1/TokenCounter.java
@@ -1,16 +1,27 @@
 package tasks.a1;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class TokenCounter {
-    private String input;
-    private Map<String, Integer> output;
+    public static void main(String[] args) {
+        System.out.println("Please enter quit or a line of tokens to be processed separated by spaces");
+        Scanner scanner = new Scanner(System.in);
+        String[] inputs = scanner.nextLine().split(" ");
+        Map<String, Integer> strMap = new HashMap<>(inputs.length);
+        for(String input:inputs){
+            strMap.put(input, strMap.get(input)==null?1:strMap.get(input)+1);
+        }
+        boolean first = true;
+        for(String key:strMap.keySet()){
+            if(!first){
+                System.out.print(",");
+            } else {
+                first = false;
+            }
+            System.out.print(key + "=" + strMap.get(key));
+        }
 
-    public void setInput(String input) {
-        this.input = input;
-    }
-
-    public Map<String, Integer> getOutput() {
-        return output;
     }
 }
