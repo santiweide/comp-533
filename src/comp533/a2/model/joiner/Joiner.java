ea59f2248a8d033f287388f71febd14f1530d863
santiweide <ggy_778@sina.com>
2021-03-20 11:00:48 +0800

    fix join

--- src/comp533/a2/model/joiner/Joiner.java
+++ src/comp533/a2/model/joiner/Joiner.java
@@ -5,31 +5,40 @@ package comp533.a2.model.joiner;
  */
 public class Joiner implements IJoiner {
     int count;
-    int nowCount;
+    int finishCount;
+    int waitCount;
 
     /**
      * @param count how many threads will call finished()
      */
     public Joiner(int count) {
         this.count = count;
-        nowCount = 0;
+        finishCount = 0;
+        waitCount = 0;
     }
 
     @Override
     public synchronized void finished() {
-        nowCount ++;
+        System.out.println("finish"+" "+Thread.currentThread().getName() + " : " + finishCount +":"+waitCount);
+        finishCount ++;
+        if(waitCount !=0 && finishCount >= count){
+            notify();
+            waitCount --;
+        }
     }
 
     @Override
     public synchronized void join() {
-        System.out.println(Thread.currentThread().getName() + " : " + nowCount);
+        System.out.println("join" + " "+Thread.currentThread().getName() + " : " + finishCount +":"+waitCount);
         try {
-            if(nowCount == count){
-                for(int i = 0;i < count;i ++){
+            if(finishCount >= count){
+                for(int i = 0;i < finishCount;i ++){
                     notify();
                 }
-                nowCount = 0;
+                finishCount = 0;
+                waitCount = 0;
             } else {
+                waitCount ++;
                 wait();
             }
         } catch (Exception e) {
